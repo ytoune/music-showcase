3520cb064166eea913f3966ae642b742d9fd2107
toune <ytoune@users.noreply.github.com>
2021-05-13 16:25:38 +0900

    Updates

--- _next/static/ClnjJ5Dhw9paUSgz_Y6Vg/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002F1ec62f68.0777479e01e697b8fce4.js","static\u002Fchunks\u002Fpages\u002Findex-a7f48f2ecb4b4649a71f.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-4c2b48bf44fd43d30c81.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
