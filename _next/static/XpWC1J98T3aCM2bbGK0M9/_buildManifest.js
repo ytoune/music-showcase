5f4bf2122305e3c8de620a135496e06c39934f41
toune <ytoune@users.noreply.github.com>
2021-12-09 15:17:12 +0900

    Updates

---
+++ _next/static/XpWC1J98T3aCM2bbGK0M9/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/144-2431cd68544fc0a6.js","static/chunks/pages/index-e070e148fe27e31e.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
