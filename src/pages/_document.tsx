7a36c1d61803ad09b0d1d8ca16efb28af7b6fbf3
toune <ytoune@users.noreply.github.com>
2022-05-29 13:53:23 +0900

    emotion のキャッシュ周り修正

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,11 +1,15 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
 import { ServerStyleSheets } from '@mui/styles'
+import createEmotionServer from '@emotion/server/create-instance'
+import createEmotionCache from '~/theme/create-emotion-cache'
 
 export default class MyDocument extends Document {
 	render() {
+		// eslint-disable-next-line @typescript-eslint/no-explicit-any
+		const { emotionStyleTags } = this.props as any
 		return (
 			<Html lang="ja">
-				<Head />
+				<Head>{emotionStyleTags}</Head>
 				<body>
 					<Main />
 					<NextScript />
@@ -19,12 +23,27 @@ MyDocument.getInitialProps = async ctx => {
 	const sheets = new ServerStyleSheets()
 	const originalRenderPage = ctx.renderPage
 
+	const cache = createEmotionCache()
+	const { extractCriticalToChunks } = createEmotionServer(cache)
+
 	ctx.renderPage = () =>
 		originalRenderPage({
-			enhanceApp: App => props => sheets.collect(<App {...props} />),
+			enhanceApp: App => props =>
+				// @ts-expect-error: ignore
+				sheets.collect(<App emotionCache={cache} {...props} />),
 		})
 
 	const initialProps = await Document.getInitialProps(ctx)
+
+	const emotionStyles = extractCriticalToChunks(initialProps.html)
+	const emotionStyleTags = emotionStyles.styles.map(style => (
+		<style
+			data-emotion={`${style.key} ${style.ids.join(' ')}`}
+			key={style.key}
+			// // eslint-disable-next-line react/no-danger
+			dangerouslySetInnerHTML={{ __html: style.css }}
+		/>
+	))
 
 	return {
 		...initialProps,
@@ -34,5 +53,6 @@ MyDocument.getInitialProps = async ctx => {
 				{sheets.getStyleElement()}
 			</>
 		) as unknown as React.ReactFragment,
+		emotionStyleTags,
 	}
 }
